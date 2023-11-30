f1c398de28b18b4669834e535a4eaf274e881859
Randall Coding <randallcoding@protonmail.com>
2023-11-30 11:39:41 +0700

    Fixes sidecar permissions for 'nobody'

--- sidecar.sh
+++ sidecar.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Running volume permissions sidecar.sh"
-adduser -uid 1000 --disabled-password --gecos "" user 
-chown -R 1000:1000 /prometheus
+chown -R 65534:65534 /prometheus
 echo "Permissions changed"
 exit 0
